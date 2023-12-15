ea25127f66bf488e6ec5cdb720bd8ff6e6034c87
yangyyou <wuyoyang@gmail.com>
2023-12-15 17:03:07 +0800

    项目添加mikro-orm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { MikroOrmConfig } from './mikro-orm.config';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import configuration from './conf/config.dev';
+import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [],
+  imports: [
+    MikroOrmModule.forRoot(MikroOrmConfig),
+    ConfigModule.forRoot({ load: [configuration], isGlobal: true }),
+    UserModule,
+    SharedModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,37 @@
+import { TsMorphMetadataProvider } from '@mikro-orm/reflection';
+import { SqlHighlighter } from '@mikro-orm/sql-highlighter';
+import { Logger } from '@nestjs/common';
+import { TSMigrationGenerator } from '@mikro-orm/migrations';
+import { defineConfig } from '@mikro-orm/core';
+import { MySqlDriver } from '@mikro-orm/mysql';
+
+const logger = new Logger('MikroOrm');
+export const MikroOrmConfig = defineConfig({
+  type: 'mysql',
+  driver: MySqlDriver,
+  host: 'localhost',
+  port: 3306,
+  dbName: 'region_shop',
+  user: 'root',
+  password: 'root',
+  metadataProvider: TsMorphMetadataProvider,
+
+  entities: [__dirname + '/../dist/**/*.entity.js'],
+  entitiesTs: [__dirname + '/../src/**/*.entity.ts'],
+  baseDir: process.cwd(),
+
+  debug: true,
+  logger: logger.log.bind(logger),
+  highlighter: new SqlHighlighter(),
+
+  migrations: {
+    tableName: 'mikro_orm_migrations',
+    emit: 'ts',
+    path: __dirname + '/../dist/migrations',
+    pathTs: __dirname + '/../src/migrations',
+    glob: '!(*.d).{js,ts}',
+    generator: TSMigrationGenerator,
+  },
+});
+
+export default MikroOrmConfig;
